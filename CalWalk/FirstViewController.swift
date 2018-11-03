9190e6764980df5a487237c5dcf7cd326134ebef
Sophia Song <38788495+songsophia@users.noreply.github.com>
2018-11-03 12:35:58 -0700

    Add files via upload

---
+++ CalWalk/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CalWalk
+//
+//  Created by Sophia Song on 11/3/18.
+//  Copyright © 2018 KASE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CalWalk/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CalWalk
+//
+//  Created by Sophia Song on 11/3/18.
+//  Copyright © 2018 KASE. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
